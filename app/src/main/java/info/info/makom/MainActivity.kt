883ecb72a5b984c8feb9ac65c565078e43d01e71
kareem <kareemmansy123@gmail.com>
2021-03-15 18:56:45 +0200

    changed base url

---
+++ app/src/main/java/info/info/makom/MainActivity.kt
@@ -0,0 +1,30 @@
+package info.info.makom
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        openWebView()
+
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun openWebView() {
+        webview.webChromeClient = WebChromeClient()
+        webview.webViewClient = WebViewClient()
+        webview.settings.javaScriptEnabled = true
+        webview.settings.setSupportMultipleWindows(true)
+        webview.loadUrl(getString(R.string.app_link))
+    }
+
+}
